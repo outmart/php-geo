d1b4449e2c1581c66e25c5280f307ddd18db3929
Karim Mohamed <komicho1996@gmail.com>
2023-01-08 17:44:52 +0200

    Feat: create a list of countries

---
+++ src/Traits/HasNamesValues.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OutMart\GEO\Traits;
+
+trait HasNamesValues
+{
+    public static function names(): array
+    {
+        return array_column(self::cases(), 'name');
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
